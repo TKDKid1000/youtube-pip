b855e76296a8f9b69b753782394fcf56d0ef909c
TKDKid1000 <69736165+TKDKid1000@users.noreply.github.com>
2020-11-21 00:06:49 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function search() {
+  var nameValue = document.getElementById("uniqueID").value.replace("https://m.youtube.com/watch?v=", "https://youtube.com/embed/");
+  var remove_after= nameValue.indexOf('&');
+  var result = nameValue.substring(0, remove_after);
+  document.getElementById("url").src = nameValue
+}
