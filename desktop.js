e118f773f9581e9723d226ac738603817772374f
TKDKid1000 <69736165+TKDKid1000@users.noreply.github.com>
2021-03-01 12:30:49 -0800

    added www

--- desktop.js
+++ desktop.js
@@ -1,5 +1,5 @@
 function search() {
-  var nameValue = document.getElementById("uniqueID").value.replace("https://youtube.com/watch?v=", "https://youtube.com/embed/");
+  var nameValue = document.getElementById("uniqueID").value.replace("https://www.youtube.com/watch?v=", "https://youtube.com/embed/");
   var remove_after= nameValue.indexOf('&');
   var result = nameValue.substring(0, remove_after);
   document.getElementById("url").src = nameValue
